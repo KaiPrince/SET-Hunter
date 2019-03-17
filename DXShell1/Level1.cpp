4c52045a5c149a571e5cba6cc740e2831e68541b
Kai Prince <Kprince2807@conestogac.on.ca>
2019-03-17 16:39:56 -0400

    Added Debug code

--- DXShell1/Level1.cpp
+++ DXShell1/Level1.cpp
@@ -42,23 +42,26 @@ void Level1::Render()
 
 	player->Draw();
 
-	char ScoreMessage[500] = "";
+	/*char ScoreMessage[500] = "";
 	gfx->WriteText(0, 0, ScoreMessage, sprintf_s(ScoreMessage, 500, "Score: %d\n Lives %d\n",
-		GameController::GetScore(), GameController::GetLives()));
+		GameController::GetScore(), GameController::GetLives()));*/
 
 
 	//DEBUG
-	/*int sX = 0;
+	int sX = 0;
 	int sY = 0;
 	Square* test = gb->FindSquare(player->GetXPos() + (player->GetWidth() / 2), player->GetYPos());
 	if (test != nullptr) {
-		sX = test->GetX();
-		sY = test->GetY();
+		sX = test->GetGbX();
+		sY = test->GetGbY();
 	}
+	
+	gfx->DrawRect(test->GetXPos(), test->GetYPos(), (float)gb->squareWidth, (float)gb->squareHeight, 1.0f, 0.0f, 0.0f, 1.0f);
+
 
 	char msg[500] = "";
 	gfx->WriteDebugText(msg, sprintf_s(msg, 500, "x: %d\n"
-		"y: %d\n sX: %d\n sY: %d\n mX: %d\n mY: %d\n", (int)player->GetXPos(), (int)player->GetYPos(), sX, sY, gfx->MouseX, gfx->MouseY));*/
+		"y: %d\n sX: %d\n sY: %d\n mX: %d\n mY: %d\n", (int)player->GetXPos(), (int)player->GetYPos(), sX, sY, gfx->MouseX, gfx->MouseY));
 }
 
 void Level1::Notify(Observable* subject) {
